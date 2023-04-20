b1e9111e3daeacb89c07a198aa448a70edd847c4
bbbgan <2893129936@qq.com>
2023-04-20 14:54:54 +0800

    adjust project structure

--- build.sh
+++ build.sh
@@ -10,13 +10,12 @@ INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
 STUB_FORMATTER="echo"
 
-# rm -rf $BUILD_DIR \
-        mkdir -p $BUILD_DIR/$BUILD_TYPE \
-        && cd $BUILD_DIR/$BUILD_TYPE \
-        && cmake \
-            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-            -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
-            -DCMAKE_STUB_FORMATTER=$STUB_FORMATTER \
-            $SOURCE_DIR \
-        && make $*
+mkdir -p $BUILD_DIR/$BUILD_TYPE \
+    && cd $BUILD_DIR/$BUILD_TYPE \
+    && cmake \
+        -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
+        -DCMAKE_STUB_FORMATTER=$STUB_FORMATTER \
+        $SOURCE_DIR \
+    && make $*
