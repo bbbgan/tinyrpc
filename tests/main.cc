09c2dddeea49dfe5d53e6a18c07393cd85b55259
bbbgan <2893129936@qq.com>
2023-04-23 01:46:01 +0800

    test

--- tests/main.cc
+++
@@ -1,5 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT
-
-#include "doctest.h"
-
-int main(int argc, char** argv) { return doctest::Context(argc, argv).run(); }
---
+++ tests/test_tinyrpc.cc
@@ -0,0 +1,5 @@
+#include "doctest.h"
+
+int main(int argc, char **argv) {
+  return doctest::Context(argc, argv).run();
+}
